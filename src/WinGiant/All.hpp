91da85865ab0b9db41d921c70abd72062ba5c046
kasicass <kasicass@gmail.com>
2016-12-28 12:05:29 +0800

    minor fix

--- src/WinGiant/All.hpp
+++ src/WinGiant/All.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 // 3rd party
-#pragma warning(disable: 4244)  // conversion from 'double' to 'float', possible loss of data
 #include "MSGPACK/include/msgpack.hpp"
 // msgpack.hpp undefined the min/max macros, take care of including something relies on min/max (e.g., <GdiplusTypes.h>) after including this file
 
